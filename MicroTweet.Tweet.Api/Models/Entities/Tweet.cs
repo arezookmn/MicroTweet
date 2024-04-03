a1c7a83d84d9210ab7560dcf8a711935c27104e4
arezookmn <arezookmn1@gmail.com>
2024-04-03 22:22:11 +0330

    Add Tweet entity

---
+++ MicroTweet.Tweet.Api/Models/Entities/Tweet.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace MicroTweet.Tweets.Api.Models.Entities;
+
+public class Tweet
+{
+    public ObjectId Id { get; init; }
+    public string Content { get; init; }
+    public DateTimeOffset CreatedOn { get; init; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset UpdatedOn { get; init; } 
+    public Guid UserId { get; init; }
+
+}
