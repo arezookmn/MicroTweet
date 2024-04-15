4fbaa29861fbf6fdab48ae07ba583c50c05047ed
arezookmn <arezookmn1@gmail.com>
2024-04-15 14:02:52 +0330

    Add extention methods for service collection

---
+++ MicroTweet.Tweet.Api/Extentions/ServiceCollectionExtentions.cs
@@ -0,0 +1,66 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Http;
+using MicroTweet.Tweets.Api.BackgrondServices;
+using MicroTweet.Tweets.Api.Models.BackgroundQueuesContext;
+using MicroTweet.Tweets.Api.Models;
+using MicroTweet.Tweets.Api.Persistence.Context;
+using MicroTweet.Tweets.Api.ServiceContracts;
+using MicroTweet.Tweets.Api.Services;
+using System.Collections.Concurrent;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+
+namespace MicroTweet.Tweets.Api.Extentions;
+
+public static class ServiceCollectionExtentions
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddHttpContextAccessor();
+
+        services.AddScoped<IUserPrinciple, UserPrinciple>(sp =>
+        {
+            var httpAccessor = sp.GetRequiredService<IHttpContextAccessor>();
+            var httpContext = httpAccessor.HttpContext;
+            return new UserPrinciple(httpContext.Connection.RemoteIpAddress.ToString());
+        });
+
+        services.AddScoped<ITweetServices, TweetService>();
+    }
+
+    public static void RegisterBackgroundServices(this IServiceCollection services)
+    {
+        services.AddHostedService<CreatedTweetBackgroundService>();
+        services.AddSingleton<ConcurrentQueue<CreatedTweetContext>>();
+
+    }
+
+    public static void RegisterMapster(this IServiceCollection services)
+    {
+        var config = TypeAdapterConfig.GlobalSettings;
+        config.Scan(Assembly.GetExecutingAssembly());
+        services.AddMapster();
+    }
+
+    public static void RegisterDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.Configure<TweetDatabaseSettings>(
+            configuration.GetSection("TweetDatabase"));
+
+        services.AddDbContext<TweetDbContext>(option =>
+        {
+            var settings = configuration.GetSection("TweetDatabase").Get<TweetDatabaseSettings>();
+            option.UseMongoDB(settings.ConnectionString, settings.DatabaseName);
+        });
+    }
+
+}
+
+
+
+
+
--- MicroTweet.Tweet.Api/Program.cs
+++ MicroTweet.Tweet.Api/Program.cs
@@ -1,50 +1,18 @@
-using FluentValidation;
-using Mapster;
-using Microsoft.EntityFrameworkCore;
-using MicroTweet.Tweets.Api.BackgrondServices;
-using MicroTweet.Tweets.Api.Models;
-using MicroTweet.Tweets.Api.Models.BackgroundQueuesContext;
-using MicroTweet.Tweets.Api.Persistence.Context;
-using MicroTweet.Tweets.Api.ServiceContracts;
-using MicroTweet.Tweets.Api.Services;
-using System.Collections.Concurrent;
-using System.Reflection;
+
+
+using MicroTweet.Tweets.Api.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddScoped<IUserPrinciple, UserPrinciple>(sp => {
-    var httpAccessor =  sp.GetRequiredService<IHttpContextAccessor>();
-    var httpContext = httpAccessor.HttpContext;
-    return new UserPrinciple( httpContext.Connection.RemoteIpAddress.ToString());
-});
+builder.Services.RegisterServices();
+builder.Services.RegisterBackgroundServices();
+builder.Services.RegisterDbContext(builder.Configuration);
+builder.Services.RegisterMapster();
 
-
-builder.Services.AddHostedService<CreatedTweetBackgroundService>();
-builder.Services.AddSingleton<ConcurrentQueue<CreatedTweetContext>>();
-
-
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
-var config = TypeAdapterConfig.GlobalSettings;
-config.Scan(Assembly.GetExecutingAssembly());
-builder.Services.AddMapster();
-
-builder.Services.AddScoped<ITweetServices, TweetService>();
-
-builder.Services.Configure<TweetDatabaseSettings>(
-    builder.Configuration.GetSection("TweetDatabase"));
-
-builder.Services.AddDbContext<TweetDbContext>( option =>
-{
-    var settings = builder.Configuration.GetSection("TweetDatabase").Get<TweetDatabaseSettings>();
-    option.UseMongoDB(settings.ConnectionString, settings.DatabaseName);
-});
 
 var app = builder.Build();
 
